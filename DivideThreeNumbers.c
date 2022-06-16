81a3726ffa364bc77031c078adc2cebee2063c88
Guru <107530258+Guru0206@users.noreply.github.com>
2022-06-16 17:32:07 +0530

    Create DivideThreeNumbers.c

---
+++ DivideThreeNumbers.c
@@ -0,0 +1,9 @@
+// write a program to divide 3 number and printt in on console
+#include <stdio.h>
+int divide (int a,int b, int c){
+    return a/b/c;
+}
+void main(){
+    int d=divide (3,2,4);
+    printf("%d",d);
+}
