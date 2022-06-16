cfbe97851c3aa376c96681a69ea295a1a2af5194
Jeeva <107607064+SmartJeeva@users.noreply.github.com>
2022-06-16 21:37:08 +0530

    Create Escape sequences.c

---
+++ Escape sequences.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main(){
+    printf("jee\nva");
+    printf("\n");
+    printf("jee\tva");
+    printf("\n");
+    printf("jee\bva");
+    printf("\n");
+    printf("jee\'va");
+    printf("\n");
+    printf("jee\"va");
+    printf("\n");
+    printf("jee\\va");
+    printf("\n");
+    printf("jee\vva");
+}
