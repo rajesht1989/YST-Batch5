aac6c7ac004244ae86ea9cf8141bc1d1b0bc8d37
Guru <107530258+Guru0206@users.noreply.github.com>
2022-06-16 16:47:34 +0530

    Create AddtwoNumbers.c

---
+++ AddtwoNumbers.c
@@ -0,0 +1,9 @@
+// write a program to add 2 number and print in on console
+#include <stdio.h>
+int add (int a,int b){
+    return a+b;
+}
+void main () {
+    int c=add (5,6);
+    printf("%d",c);
+}
