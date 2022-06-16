1c78c5e514d1ca14228cf605727f8e5e02910e13
Vigneshkumar2006 <107530054+Vigneshkumar2006@users.noreply.github.com>
2022-06-16 15:45:16 +0530

    Create UseAllEscapeSequnces.c

---
+++ UseAllEscapeSequnces.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main(){
+    printf("vic\nky");
+    printf("\n");
+    printf("vic\tky");
+    printf("\n");
+    printf("vic\bky");
+    printf("\n");
+    printf("vic\'ky");
+    printf("\n");
+    printf("vic\"ky");
+    printf("\n");
+    printf("vic\\ky");
+    printf("\n");
+    printf("vic\vky");
+    
+}
