3235d8d8dfdfe73318b45b95304bfae173504d1d
Inbha1503 <107529205+Inbha1503@users.noreply.github.com>
2022-06-16 20:11:31 +0530

    Create Subtract2No's.c

---
+++ Subtract2No's.c
@@ -0,0 +1,9 @@
+//write a program to subtract 2 nos and print it on console
+#include<stdio.h>
+int subtract(int a,int b){
+    return a-b;
+}
+void main(){
+    int d=subtract(3,2);
+    printf("%d",d);
+}
