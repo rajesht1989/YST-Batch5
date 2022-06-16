98ba3cabce2560fdf56f79deb38370ee7bd50383
Guru <107530258+Guru0206@users.noreply.github.com>
2022-06-16 16:57:57 +0530

    Create SubractThreeNumbers.c

---
+++ SubractThreeNumbers.c
@@ -0,0 +1,9 @@
+// write a program to add 2 number and print in on console
+#include <stdio.h>
+int subract (int a,int b,int c){
+    return a-b-c;
+}
+void main(){
+    int d=subract (10,10,10);
+    printf("%d",d);
+}
