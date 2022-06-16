f5f3c78485cfd257ec5b09a2fab4314810d3c352
K.Murugaperumal <107532867+muruga11061998@users.noreply.github.com>
2022-06-16 15:56:29 +0530

    Update EscapeSequence.c

--- EscapeSequence.c
+++ EscapeSequence.c
@@ -1,1 +1,19 @@
 
+// Wriye a program to use all escape sequences
+#include <stdio.h>
+void main(){
+    printf("Muruga\nperumal");
+    printf("\n");
+    printf("Muruga\tperumal");
+    printf("\n");
+    printf("Muruga\bperumal");
+    printf("\n");
+    printf("Muruga\'perumal");
+    printf("\n");
+    printf("Muruga\"Perumal");
+    printf("\n");
+    printf("Muruga\\perumal");
+    printf("\n");
+    printf("Muruga\vperumal");
+    printf("\n");
+}
