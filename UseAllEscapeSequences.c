6b877b7d39edd568ed8a7a4f1909b7e020afebc3
Inbha1503 <107529205+Inbha1503@users.noreply.github.com>
2022-06-16 15:42:37 +0530

    Create UseAllEscapeSequences.c

---
+++ UseAllEscapeSequences.c
@@ -0,0 +1,17 @@
+//write a program to use all escape sequences
+#include <stdio.h>
+void main(){
+    printf("na\nme");
+    printf("\n");
+    printf("na\tme");
+    printf("\n");
+    printf("na\bme");
+    printf("\n");
+    printf("na\'me");
+    printf("\n");
+    printf("na\"me");
+    printf("\n");
+    printf("na\\me");
+    printf("\n");
+    printf("na\vme");
+}
