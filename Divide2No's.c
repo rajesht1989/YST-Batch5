b06afdc0601b33969b7aa932b3cd8c9a28973bb9
Inbha1503 <107529205+Inbha1503@users.noreply.github.com>
2022-06-16 20:19:06 +0530

    Create Divide2No's.c

---
+++ Divide2No's.c
@@ -0,0 +1,9 @@
+//write a program to divide 2 nos and print it on console
+#include<stdio.h>
+int divide(int a,int b){
+    return a/b;
+}
+void main(){
+    int d=divide(10,5);
+    printf("%d",d);
+}
