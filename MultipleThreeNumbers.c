87e4d9860ce6af996eca37fd836aa12a3c23f26d
Guru <107530258+Guru0206@users.noreply.github.com>
2022-06-16 17:33:54 +0530

    Update and rename MultipleThreeNumbers to MultipleThreeNumbers.c

--- MultipleThreeNumbers.c
+++ MultipleThreeNumbers.c
@@ -1,4 +1,4 @@
-// write a program to multiple 3 number and printt in on console
+// write a program to multiple 3 number and print in on console
 #include <stdio.h>
 int subract (int a,int b, int c){
     return a*b*c;
