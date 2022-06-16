f79960201164839618c61f5f8f761b3697c4279f
Inbha1503 <107529205+Inbha1503@users.noreply.github.com>
2022-06-16 20:09:10 +0530

    Create Add3No's.c

---
+++ Add3No's.c
@@ -0,0 +1,9 @@
+//write a program to add 3 nos and print it on console
+#include<stdio.h>
+int add(int a,int b,int c){
+    return a+b+c;
+}
+void main(){
+    int d=add(3,2,5);
+    printf("%d",d);
+}
