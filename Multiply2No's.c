78cec4ba159c73649f2031b1ae9911554d33e185
Inbha1503 <107529205+Inbha1503@users.noreply.github.com>
2022-06-16 20:16:25 +0530

    Create Multiply2No's.c

---
+++ Multiply2No's.c
@@ -0,0 +1,9 @@
+//write a program to multiply 2 nos and print it on console
+#include<stdio.h>
+int multiply(int a,int b){
+    return a*b;
+}
+void main(){
+    int d=multiply(10,5);
+    printf("%d",d);
+}
