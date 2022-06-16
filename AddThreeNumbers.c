30f8ee4af5be29cd1442d4189165ca512a085df9
Guru <107530258+Guru0206@users.noreply.github.com>
2022-06-16 16:49:55 +0530

    Create AddThreeNumbers.c

---
+++ AddThreeNumbers.c
@@ -0,0 +1,9 @@
+// write a program to add 2 number and print in on console
+#include <stdio.h>
+int add (int a,int b, int c){
+    return a+b+c;
+}
+void main () {
+    int d=add (5,5,5);
+    printf("%d",d);
+}
