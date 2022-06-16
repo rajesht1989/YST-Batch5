6ac20d70676b7c90f10536055a91c276be3b21a4
Inbha1503 <107529205+Inbha1503@users.noreply.github.com>
2022-06-16 20:15:07 +0530

    Create Multiply3No's.c

---
+++ Multiply3No's.c
@@ -0,0 +1,9 @@
+//write a program to multiply 3 nos and print it on console
+#include<stdio.h>
+int multiply(int a,int b,int c){
+    return a*b*c;
+}
+void main(){
+    int d=multiply(10,1,5);
+    printf("%d",d);
+}
