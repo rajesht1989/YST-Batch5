057febecffa79a62eaacd67eb66e867f44fb8141
jprakashdrop <107497112+jprakashdrop@users.noreply.github.com>
2022-06-17 20:26:41 +0530

    Create GetANumberFromUserAndPrintItOnConsole.c

---
+++ GetANumberFromUserAndPrintItOnConsole.c
@@ -0,0 +1,11 @@
+//write a program to get a number  from user and print it on console
+#include<stdio.h>
+void main(){
+    int  b= getANumberFromUser();
+printf("%n",b);
+}
+int getANumberFromUser(){
+int g;
+scanf("%d",&g);
+return g;
+}
