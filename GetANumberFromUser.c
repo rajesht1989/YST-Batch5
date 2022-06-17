2c4968664ee2b63fcb5bfb8fc6941773a9e826d9
Inbha1503 <107529205+Inbha1503@users.noreply.github.com>
2022-06-17 18:34:05 +0530

    Create GetANumberFromUser.c

---
+++ GetANumberFromUser.c
@@ -0,0 +1,11 @@
+//write a program to get a number from user and print it on console
+#include<stdio.h>
+void main(){
+    int c=numberVangudaSomberi();
+    printf("%i",c);
+}
+int numberVangudaSomberi(){
+    int p;
+    scanf("%d",&p);
+    return p;
+}
