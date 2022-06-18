2f0e16afd1809fe4a151d6e91bf5d8321e86ffcd
Jeeva <107607064+SmartJeeva@users.noreply.github.com>
2022-06-18 07:05:48 +0530

    Create getanumberfromuser.c

---
+++ getanumberfromuser.c
@@ -0,0 +1,10 @@
+// write a program to get a number  from user and print it on console
+#include<stdio.h>
+void main(){
+    int  c= getANumberFromUser();
+printf("%i",c);
+}int getANumberFromUser(){
+int j;
+scanf("%d",&j);
+return j;
+}
