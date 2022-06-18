7f5a632cfb8bbb1edc00ea8d5120d7e0e27f1151
Jeeva <107607064+SmartJeeva@users.noreply.github.com>
2022-06-18 09:32:18 +0530

    Update getANumberFromUser.c

--- getANumberFromUser.c
+++ getANumberFromUser.c
@@ -1,9 +1,11 @@
 // write a program to get a number  from user and print it on console
 #include<stdio.h>
+
 void main(){
-    int  c= getANumberFromUser();
+int c= getANumberFromUser();
 printf("%i",c);
-}int getANumberFromUser(){
+}
+int getANumberFromUser(){
 int j;
 scanf("%d",&j);
 return j;
