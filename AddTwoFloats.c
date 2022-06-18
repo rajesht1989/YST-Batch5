2a9512eddfc6440566650f2311f365adef9b5d16
jprakashdrop <107497112+jprakashdrop@users.noreply.github.com>
2022-06-18 10:09:51 +0530

    Create AddTwoFloats.c

---
+++ AddTwoFloats.c
@@ -0,0 +1,9 @@
+//write a Program to add two floats and print it on console
+#include<stdio.h>
+float add(float a,float b){
+    return a+b;
+}
+void main(){
+    float c=add(2.5,2.5);
+    printf("%f",c);
+}
