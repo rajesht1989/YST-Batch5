996f7105009f85c7a83a9cb0fde4491058ea5c2e
jprakashdrop <107497112+jprakashdrop@users.noreply.github.com>
2022-06-19 08:43:01 +0530

    Create Multiple2No's.c

---
+++ Multiple2No's.c
@@ -0,0 +1,9 @@
+//write a program to multiply 2 numbers and print it on console
+#include<stdio.h>
+int multiply(int x,int y){
+    return x*y;
+}
+void main(){
+    int d=multiply(3,7);
+    printf("%d",d);
+}
