aeb43d8d1b6ab460680e89280c352c8ae7027aef
jprakashdrop <107497112+jprakashdrop@users.noreply.github.com>
2022-06-19 08:39:59 +0530

    Create Subtract3No's.c

---
+++ Subtract3No's.c
@@ -0,0 +1,9 @@
+// write a program to subtract 3 number and printt in on console
+#include <stdio.h>
+int subract (int x,int y,int z){
+    return x-y-z;
+}
+void main(){
+    int p=subract (8,3,2);
+    printf("%d",p);
+}
