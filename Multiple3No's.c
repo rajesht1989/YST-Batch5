91048641fe5dceef05aaad3238fd9ecd93f6bca8
jprakashdrop <107497112+jprakashdrop@users.noreply.github.com>
2022-06-19 08:45:41 +0530

    Create Multiple3No's.c

---
+++ Multiple3No's.c
@@ -0,0 +1,9 @@
+//write a program to multiply 3 numbers and print it on console
+#include<stdio.h>
+int multiply(int a,int b,int c){
+    return a*b*c;
+}
+void main(){
+    int d=multiply(6,8,7);
+    printf("%d",d);
+}
