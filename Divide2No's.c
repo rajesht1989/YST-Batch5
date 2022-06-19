ce8a2f14b8fcba2965f96bde69012fc920674841
jprakashdrop <107497112+jprakashdrop@users.noreply.github.com>
2022-06-19 12:08:58 +0530

    Create Divide2No's.c

---
+++ Divide2No's.c
@@ -0,0 +1,9 @@
+  program to divide 2 number and print it on console
+#include <stdio.h>
+int divide (int a,int b){
+    return a/b;
+}
+void main(){
+    int c=divide (8,4);
+    printf("%d",c);
+}
