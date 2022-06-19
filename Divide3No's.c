c311268f1e5a954f871b366a66851f279fba62e3
jprakashdrop <107497112+jprakashdrop@users.noreply.github.com>
2022-06-19 12:11:40 +0530

    Create Divide3No's.c

---
+++ Divide3No's.c
@@ -0,0 +1,9 @@
+  //write aprogram to divide 3 number and print it on console
+#include <stdio.h>
+int divide (int a,int b,int c){
+    return a/b/c;
+}
+void main(){
+    int d=divide (6,7,8);
+    printf("%d",d);
+}
