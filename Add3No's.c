2f91ff018f5a85ac78e161d412edba7ccbd75f58
jprakashdrop <107497112+jprakashdrop@users.noreply.github.com>
2022-06-19 08:21:23 +0530

    Create Add3No's.c

---
+++ Add3No's.c
@@ -0,0 +1,9 @@
+//write a program to add 3 nos and print it on console
+#include<stdio.h>
+int add(int a,int b,int c){
+    return a+b+c;
+}
+void main(){
+    int d=add(5,4,6);
+    printf("%d",d);
+}
