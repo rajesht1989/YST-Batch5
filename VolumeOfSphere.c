b2a2f422782955e6001e189650e6a1d52a37e1af
Inbha1503 <107529205+Inbha1503@users.noreply.github.com>
2022-06-21 20:44:51 +0530

    Create VolumeOfSphere.c

---
+++ VolumeOfSphere.c
@@ -0,0 +1,22 @@
+//write a program to get radius from user and identify the volume of sphere and print it on console
+#include<stdio.h>
+
+int getARadiusFromUser();
+float volumeOfSphere(int x);
+void main(){
+    int a=getARadiusFromUser();
+    float b = volumeOfSphere(a);
+    printf("Volume is %f",b);
+}
+
+int getARadiusFromUser(){
+    int c;
+    printf("Enter the radius of sphere ");
+    scanf("%i",&c);
+    return c;
+}
+
+float volumeOfSphere(int x) {
+    return (4/3) * 3.141592653589793238462643383279502884197 * x * x * x;
+}
+
