b2db728791c1c19925758e01a8ac841b7369db81
jeeva <107607064+SmartJeeva@users.noreply.github.com>
2022-06-23 10:50:56 +0530

    Create getRadiusFromUser.c

---
+++ getRadiusFromUser.c
@@ -0,0 +1,18 @@
+// to find area of circle,get radius as input from user
+
+#include<stdio.h>
+
+float getRadiusFromUser(){
+    float a;
+    printf ("Enter A Radius");
+    scanf("%f",&a);
+}
+float areaFromCircle(float r){
+    return(3.14*(r*r));
+}
+void main(){
+    float number=getRadiusFromUser();
+    float area=areaFromCircle(number);
+    
+    printf ("area is %f ",area);
+}
