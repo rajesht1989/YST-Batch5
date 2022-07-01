b21cb0dc8c463a1e9a583952cee9095f956d0fc2
K.Murugaperumal <107532867+muruga11061998@users.noreply.github.com>
2022-07-01 11:56:31 +0530

    Create GetANumberAddision.c

---
+++ GetANumberAddision.c
@@ -0,0 +1,17 @@
+//write a program to get a number from user print it on console
+#include<stdio.h>
+int getNumberFromUser(){
+    int number;
+    printfEnter the number");
+    scanf("%d",& number);
+    return number;
+}
+int addisionTwoNumbers(int a,int b){
+    return a+b;
+}
+void main(){
+    int a=();
+    int b=getNumberFromUser();
+    int addision=addisionTwoNumbers(a,b);
+    printf("Answer is %d",addision);
+}
