720ea7feb84637ee9ec6abddf62cec492f9255eb
K.Murugaperumal <107532867+muruga11061998@users.noreply.github.com>
2022-07-02 16:11:54 +0530

    Create elseIfLetter.c

---
+++ elseIfLetter.c
@@ -0,0 +1,48 @@
+//write a program get a to z letters from user print it on console.
+#include<stdio.h>
+
+char getLetterFromUser(){
+  char letter;
+      printf("Enter a letter");
+      scanf("%c",& letter);
+return letter;
+}
+void letter(char Name ){
+  if (Name=='a'||Name=='A'){
+      printf("Anitha");
+  }
+  else if(Name=='b'||Name=='B'){
+      printf("Bharathi");
+  }
+  else if(Name=='c'||Name=='C'){
+      printf("Chantra");
+  }
+  else if(Name=='d'||Name=='D'){
+      printf("Devi");
+  }
+  else if(Name=='e'||Name=='E'){
+      printf("Eswari");
+  }
+  else if(Name=='f'||Name=='F'){
+      printf("faritha");
+  }
+  else if(Name=='g'||Name=='G'){
+      printf("Gowsalya");
+  }
+  else if(Name=='h'||Name=='H'){
+      printf("Harini");
+  }
+  else if(Name=='i'||Name=='I'){
+      printf("Ilakya");
+  }
+  else if(Name=='j'||Name=='J'){
+      printf("Jeevitha");
+  }
+  else{
+      printf("No Added");
+  }
+}
+ void main(){
+ 
+ letter(getLetterFromUser());
+}
