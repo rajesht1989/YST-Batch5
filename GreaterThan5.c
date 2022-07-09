37a88eb8720fbc879b8edb5d6679601750fada00
Inbha1503 <107529205+Inbha1503@users.noreply.github.com>
2022-07-09 16:32:27 +0530

    Create GreaterThan5.c

---
+++ GreaterThan5.c
@@ -0,0 +1,23 @@
+//write a program to get a number from user and identify it is greater than 5
+#include<stdio.h>
+int getNum(){
+    int a;
+    printf("Enter a number : ");
+    scanf("%d",&a);
+    return a;
+}
+void greaterThan(int a){
+    if(a>5){
+        printf("The given number is Greater than five");
+    }
+    else if(a<{
+        printf("The given number is not Greater than five");
+    }
+    else{
+        printf("Equal");
+    }
+}
+void main(){
+    int a=getNum();
+    greaterThan(a);
+}
