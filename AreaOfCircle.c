6dfe19a3a29aa97928bb3247b88a7890598ef590
Guru <107530258+Guru0206@users.noreply.github.com>
2022-07-14 19:22:16 +0530

    Create AreaOfCircle.c

---
+++ AreaOfCircle.c
@@ -0,0 +1,8 @@
+//write a program to get radius from user and identify the Area and Circumference of circle
+#include<stdio.h>
+int getANumberFromUser(){
+int n;
+printf("enter a nuumber");
+scanf("%d",i);
+return i;
+}
