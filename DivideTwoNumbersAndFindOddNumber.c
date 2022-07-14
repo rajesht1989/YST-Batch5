a29b2cb242d7744a8c8a8c3bd6d9c83ff16f9b06
Inbha1503 <107529205+Inbha1503@users.noreply.github.com>
2022-07-14 19:38:50 +0530

    Create DivideTwoNumbersAndFindOddNumber.c

---
+++ DivideTwoNumbersAndFindOddNumber.c
@@ -0,0 +1,26 @@
+//write a program to get 2 number from user and divide them and identify if only odd
+#include<stdio.h>
+int getNum(){
+    int a;
+    printf("Enter a number : ");
+    scanf("%d",&a);
+    return a;
+}
+int divide(int a,int b){
+    return a/b;
+}
+void identifyOddNumber(int c){
+    if(c%2==0){
+        printf("Given Number is Not odd number");
+    }
+    else{
+        printf("Given Number is Odd number");
+    }
+}
+void main(){
+    int a=getNum();
+    int b=getNum();
+    int c=divide(a,b);
+    printf("Answer is %d\n",c);
+    identifyOddNumber(c);
+}
