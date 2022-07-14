39d6c122529244661b6144e5734f67956dbd66b6
Inbha1503 <107529205+Inbha1503@users.noreply.github.com>
2022-07-14 17:48:04 +0530

    Update PrimeNumbers20.c

--- PrimeNumbers20.c
+++ PrimeNumbers20.c
@@ -1,5 +1,41 @@
 //write a program to print prime numbers below 20
 #include<stdio.h>
+int getNum(){
+    int a;
+    printf("Enter the Number only below 20 : ");
+    scanf("%d",&a);
+    return a;
+}
+void printPrimeNum(int a){
+    if(a==2){
+        printf("Prime Number");
+    }
+    else if(a==3){
+        printf("Prime Number");
+    }
+    else if(a==5){
+        printf("Prime Number");
+    }
+    else if(a==7){
+        printf("Prime Number");
+    }
+    else if(a==11){
+        printf("Prime Number");
+    }
+    else if(a==13){
+        printf("Prime Number");
+    }
+    else if(a==17){
+        printf("Prime Number");
+    }
+    else if(a==19){
+        printf("Prime Number");
+    }
+    else{
+        printf("Its not prime number");
+    }
+}
 void main(){
-    printf("Prime numbers that below 20 are 2, 3, 5, 7, 11, 13, 17, 19");
+    int a=getNum();
+    printPrimeNum(a);
 }
