5eaa12de36645c9aef447dc9eafd8c5d05c3a265
Guru <107530258+Guru0206@users.noreply.github.com>
2022-07-15 12:37:17 +0530

    Create AddTwoNumbersAndFindEvenNumbers.c

---
+++ AddTwoNumbersAndFindEvenNumbers.c
@@ -0,0 +1,26 @@
+//write a program to get 2 number from user and divide them and identify if only even
+#include<stdio.h>
+int getNumberFromUser(){
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    return n;
+}
+int addTheNumbers(int n,int m){
+    return n+m;
+}
+void identifyEvenNumber(int z){
+    if(z%2==0){
+        printf("Given Number is even r");
+    }
+    else{
+        printf("Given Number is Not even ");
+    }
+}
+void main(){
+    int n=getNumberFromUser();
+    int m=getNumberFromUser();
+    int z=addTheNumbers(n,m);
+    printf("Answer is %d\n",z);
+    identifyEvenNumber(z);
+}
