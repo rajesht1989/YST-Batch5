db329833a52d0798dbff090165a506ec33101787
Inbha1503 <107529205+Inbha1503@users.noreply.github.com>
2022-07-15 09:15:43 +0530

    Create ModuleTwoNumbersAndFindEvenNumber.c

---
+++ ModuleTwoNumbersAndFindEvenNumber.c
@@ -0,0 +1,26 @@
+//write a program to get 2 number from user and module them and identify if only even
+#include<stdio.h>
+int getNum(){
+    int a;
+    printf("Enter a number : ");
+    scanf("%d",&a);
+    return a;
+}
+int module(int a,int b){
+    return a%b;
+}
+void identifyEvenNumber(int c){
+    if(c%2==0){
+        printf("Given Number is even number");
+    }
+    else{
+        printf("Given Number is Not even number");
+    }
+}
+void main(){
+    int a=getNum();
+    int b=getNum();
+    int c=module(a,b);
+    printf("Answer is %d\n",c);
+    identifyEvenNumber(c);
+}
