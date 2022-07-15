61253ecfe40ea72209664519f3947e639ac0f0c0
Guru <107530258+Guru0206@users.noreply.github.com>
2022-07-15 12:45:44 +0530

    Create AddThreeNumbersAndIdentifyItsIsOdd.c

---
+++ AddThreeNumbersAndIdentifyItsIsOdd.c
@@ -0,0 +1,27 @@
+//write a program to get Three number from user and Add Them them and identify it is odd number
+#include<stdio.h>
+int getNumberFromUser(){
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    return n;
+}
+int addTheNumbers(int n,int m,int o){
+    return n+m+o;
+}
+void identifyEvenNumber(int z){
+    if(z%2==0){
+        printf("Given Number is Not odd");
+    }
+    else{
+        printf("Given Number is Odd ");
+    }
+}
+void main(){
+    int n=getNumberFromUser();
+    int m=getNumberFromUser();
+    int o=getNumberFromUser();
+    int z=addTheNumbers(n,m,o);
+    printf("Answer is %d\n",z);
+    identifyEvenNumber(z);
+}
