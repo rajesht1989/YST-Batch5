23c9f406903108ec8abfd0ba81137af0453f6a30
K.Murugaperumal <107532867+muruga11061998@users.noreply.github.com>
2022-07-17 18:49:54 +0530

    Create addisionOddThreeNumbers.c

---
+++ addisionOddThreeNumbers.c
@@ -0,0 +1,27 @@
+//write a program to get three number from user identify the ODD number and print it on console. 
+#include<stdio.h>
+int getNumber(){
+    int a;
+    printf("Enter a number : ");
+    scanf("%d",&a);
+    return a;
+}
+int addision(int a,int b,int c){
+    return a+b+c;
+}
+void identifyOddNumber(int d){
+    if(d%2==1){
+        printf("The number is ODD number");
+    }
+    else{
+        printf("The Number is NOT ODD number");
+    }
+}
+void main(){
+    int a=getNumber();
+    int b=getNumber();
+    int c=getNumber();
+    int d=addision(a,b,c);
+    printf("Answer is %d\n",d);
+    identifyOddNumber(d);
+}
