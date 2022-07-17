47aef2db0524ffe112687b895f0a05f68f4b6a24
K.Murugaperumal <107532867+muruga11061998@users.noreply.github.com>
2022-07-17 17:49:10 +0530

    Create divideOddTwoNumbers.c

---
+++ divideOddTwoNumbers.c
@@ -0,0 +1,26 @@
+//write a program to get two number from user identify the ODD number and print it on console. 
+#include<stdio.h>
+int getNum(){
+    int a;
+    printf("Enter a number : ");
+    scanf("%d",&a);
+    return a;
+}
+int divide(int a,int b){
+    return a/b;
+}
+void identifyOddNumber(int c){
+    if(c%2==1){
+        printf("The number is ODD number");
+    }
+    else{
+        printf("The Number is NOT ODD number");
+    }
+}
+void main(){
+    int a=getNum();
+    int b=getNum();
+    int c=divide(a,b);
+    printf("Answer is %d\n",c);
+    identifyOddNumber(c);
+}
