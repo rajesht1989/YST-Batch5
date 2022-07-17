59567ec46dfc0f1dca8179c52cbb30f3a4e2f176
K.Murugaperumal <107532867+muruga11061998@users.noreply.github.com>
2022-07-17 19:08:05 +0530

    Create multipleEvenThreeNumber.c

---
+++ multipleEvenThreeNumber.c
@@ -0,0 +1,27 @@
+//write a program to get three number from user identify the EVEN number and print it on console. 
+#include<stdio.h>
+int getNumber(){
+    int a;
+    printf("Enter a number : ");
+    scanf("%d",&a);
+    return a;
+}
+int multiple(int a,int b,int c){
+    return a*b*c;
+}
+void identifyEvenNumber(int d){
+    if(d%2==0){
+        printf("The number is EVEN number");
+    }
+    else{
+        printf("The Number is NOT EVEN number");
+    }
+}
+void main(){
+    int a=getNumber();
+    int b=getNumber();
+    int c=getNumber();
+    int d=multiple(a,b,c);
+    printf("Answer is %d\n",d);
+    identifyEvenNumber(d);
+}
