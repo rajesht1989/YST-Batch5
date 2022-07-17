bbfdfcda0a5e64aa1942c71d1ce5b41249847ece
K.Murugaperumal <107532867+muruga11061998@users.noreply.github.com>
2022-07-17 17:07:37 +0530

    Create addisionEvenTwoNumbers.c

---
+++ addisionEvenTwoNumbers.c
@@ -0,0 +1,26 @@
+//write a program to get two number from user identify the EVEN number and print it on console. 
+#include<stdio.h>
+int getNum(){
+    int a;
+    printf("Enter a number : ");
+    scanf("%d",&a);
+    return a;
+}
+int addition(int a,int b){
+    return a+b;
+}
+void identifyEvenNumber(int c){
+    if(c%2==0){
+        printf("The number is EVEN number");
+    }
+    else{
+        printf("The Number is NOT EVEN number");
+    }
+}
+void main(){
+    int a=getNum();
+    int b=getNum();
+    int c=addition(a,b);
+    printf("Answer is %d\n",c);
+    identifyEvenNumber(c);
+}
