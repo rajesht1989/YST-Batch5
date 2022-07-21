0886394ede83bf5547d57daf07393c5615f359fd
Guru <107530258+Guru0206@users.noreply.github.com>
2022-07-21 15:03:17 +0530

    Create PrintEvenNumbersUsingLoop.c

---
+++ PrintEvenNumbersUsingLoop.c
@@ -0,0 +1,15 @@
+//write a Program to get a number from user Print Even Numbers 
+#include<stdio.h>
+int main(){
+  	int i ;
+  	printf(" Enter number: ");
+  	scanf("%d", & i);
+  	printf("\n Even Numbers between 1 and %d are : \n",i);
+  	
+  	for(i = 1; i <= i; i++){
+    	if ( i % 2 == 0 ) {
+  			printf(" %d\n",i);
+    	}
+  	}
+  	return 0;
+}
