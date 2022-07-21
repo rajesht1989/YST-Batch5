431b1efb7d20c41d026ae29586a911053f29bed0
Guru <107530258+Guru0206@users.noreply.github.com>
2022-07-21 17:02:13 +0530

    Create PrintGuruUsingLoop.c

---
+++ PrintGuruUsingLoop.c
@@ -0,0 +1,17 @@
+//write a program to get number from user and print name on console
+#include<stdio.h>
+int getNumberFromUser(){
+    int number;
+    printf("Enter a number : ");
+    scanf("%d",&number);
+    return number;
+}
+void printName(int number){
+    for(int i=1;i<=number;i++){
+        printf("guru\n");
+    }
+}
+void main(){
+    int number=getNumberFromUser();
+    printName(number);
+}
