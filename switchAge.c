8e944310f2aea871b0174d6240eaf0f6f96eb4f4
K.Murugaperumal <107532867+muruga11061998@users.noreply.github.com>
2022-07-22 16:48:54 +0530

    Create switchAge.c

---
+++ switchAge.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int getAgeFromUser(){
+    int age;
+    printf("Enter a age");
+    scanf("%d",&age);
+    return age;
+}
+int getAge(int age){
+switch(age){
+case 1:
+case 2:
+case 3:
+printf("Infant");
+break;
+case 4:
+case 5:
+case 6:
+printf("kid");
+break;
+default:
+printf("Men");
+break;
+}
+}
+void main(){
+    getAge(getAgeFromUser());
+}
+
+    
