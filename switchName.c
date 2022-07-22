ced7be77ebddc3a7822fc206cf4583bdf1e3d99f
K.Murugaperumal <107532867+muruga11061998@users.noreply.github.com>
2022-07-22 17:38:34 +0530

    Create switchName.c

---
+++ switchName.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+char getNameFromUser(){
+    char n;
+    printf("Enter a name : ");
+    scanf("%c",&n);
+    return n;
+}
+char identifyName(int n){
+    switch(n){
+    case 'a':
+    case 'A':
+    printf("Anjali");
+    break;
+    case 'b':
+    case 'B':
+    printf("Brindha");
+    break;
+    case 'c':
+    case 'C':
+    printf("Chitra");
+    break;
+    case 'd':
+    case 'D':
+    printf("divya");
+    break;
+    case 'e':
+    case 'E':
+    printf("Eswari");
+    break;
+default:
+printf("no name");
+break;
+}
+}
+void main(){
+ char n = getNameFromUser(); identifyName(n);
+}
+
+    
