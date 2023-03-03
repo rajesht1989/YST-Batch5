ac7b181ea8d38922e13ccf4d8f98db4db974eba8
jeeva <107607064+Jeeva0604@users.noreply.github.com>
2023-03-03 10:17:11 +0530

    Update areaOfCircle.c

--- areaOfCircle.c
+++ areaOfCircle.c
@@ -11,7 +11,10 @@ float areaFromCircle(float r){
     return(3.14*(r*r));
 }
 void main(){
+     Printf("Enter The Radius");
+
     float number=getRadiusFromUser();
+
     float area=areaFromCircle(number);
     
     printf ("area is %f ",area);
